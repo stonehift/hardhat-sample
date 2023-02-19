0190b49c5e7b247cee8e943b83aadaf4419b413a
stonehift <125779722+stonehift@users.noreply.github.com>
2023-02-19 11:52:28 +0800

    Update FT.sol

--- contracts/FT.sol
+++ contracts/FT.sol
@@ -2,29 +2,43 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract FT is ERC20, Pausable, Ownable {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
-    
+contract FT is ERC20 {
+
+    address internal owner;
+
+    bool internal paused = false;
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = msg.sender;
+    }
+
+    function mint(address account, uint256 amount) external {
+    modifier onlyOwner(){
+        require(owner == msg.sender);
+        _;
+    }
+
+    modifier pause(){
+        require(paused == false);
+        _;
+    }
+
     function mint(address account, uint256 amount) external onlyOwner {
-        _mint(account, amount);
+        _mint(account,amount);
     }
 
     function burn(uint256 amount) external {
-        _burn(msg.sender, amount);
+
+        _burn(msg.sender,amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override whenNotPaused {
-        super._beforeTokenTransfer(from, to, amount);
+    function transfer(address to,uint amount) public override pause  returns(bool) {
+        address from = _msgSender();
+        _transfer(from, to, amount);
+        return true;
     }
-    
-    function pause() external onlyOwner {
-        _pause();
-    }
-    
-    function unpause() external onlyOwner {
-        _unpause();
+    function changePause(bool newPause) onlyOwner public returns(bool) {
+        paused = newPause;
+        return true;
     }
 }
